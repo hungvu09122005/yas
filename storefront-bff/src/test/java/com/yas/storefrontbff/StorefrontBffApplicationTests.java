c88a100d9bd0b96a2f4cd9143d1c7085cff0b14e
Hungvu2005 <vuhung09122005@gmail.com>
2026-04-28 13:48:44 +0700

    test(storefront-bff): add dummy test to generate surefire report for dorny/test-reporter

---
+++ storefront-bff/src/test/java/com/yas/storefrontbff/StorefrontBffApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yas.storefrontbff;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StorefrontBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+
+}
+
