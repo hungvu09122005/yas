12a3387278d268e15a080d8311fe6a053bdf78f0
Hungvu2005 <vuhung09122005@gmail.com>
2026-06-25 22:19:43 +0700

    feat: initialize paypal payment service with Maven configuration, test suite, and project conventions

---
+++ payment-paypal/src/test/java/com/yas/payment/paypal/PaymentPaypalApplicationTest.java
@@ -0,0 +1,14 @@
+package com.yas.payment.paypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+class PaymentPaypalApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // It is supposed to be empty
+    }
+
+}
