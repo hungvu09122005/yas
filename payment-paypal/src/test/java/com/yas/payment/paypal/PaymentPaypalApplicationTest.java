9c9708bd707b75a5a518096398dbc4863cc5fa08
Hungvu2005 <vuhung09122005@gmail.com>
2026-06-25 22:36:01 +0700

    feat: add project documentation, test boilerplate for PayPal service, and CI guidelines

--- payment-paypal/src/test/java/com/yas/payment/paypal/PaymentPaypalApplicationTest.java
+++ payment-paypal/src/test/java/com/yas/payment/paypal/PaymentPaypalApplicationTest.java
@@ -5,7 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
-@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@SpringBootTest(
+    useMainMethod = SpringBootTest.UseMainMethod.ALWAYS,
+    properties = {
+        "yas.public.url=http://api.yas.local.com/payment-paypal",
+        "spring.security.oauth2.resourceserver.jwt.issuer-uri=test"
+    }
+)
 class PaymentPaypalApplicationTest {
 
     @MockitoBean
