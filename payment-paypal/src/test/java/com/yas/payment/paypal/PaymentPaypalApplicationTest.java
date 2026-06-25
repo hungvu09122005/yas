e3abf60b204cd22350d6f7c45f7c1e20a6fdc4bc
Hungvu2005 <vuhung09122005@gmail.com>
2026-06-25 22:27:15 +0700

    feat: add project conventions and initial unit test scaffold for payment-paypal service

--- payment-paypal/src/test/java/com/yas/payment/paypal/PaymentPaypalApplicationTest.java
+++ payment-paypal/src/test/java/com/yas/payment/paypal/PaymentPaypalApplicationTest.java
@@ -2,9 +2,14 @@ package com.yas.payment.paypal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class PaymentPaypalApplicationTest {
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
